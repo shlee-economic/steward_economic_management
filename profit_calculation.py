29f397cbbad4f67f9cb9c80388095dc711974993
shlee-economic <sanghoonw0807@gmail.com>
2024-03-17 15:22:08 +0900

    check issue

--- profit_calculation.py
+++ profit_calculation.py
@@ -1,13 +1,13 @@
-
 from flask import Flask, render_template, request
 from calculator_module import calculate_investment
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template(r'index.html')
+    return render_template('index.html')
 
-@app.route('result.html', methods=['POST'])
+@app.route('/result', methods=['POST'])
 def result():
     initial_investment = float(request.form['initial_investment'])
     annual_return = float(request.form['annual_return'])
@@ -24,7 +24,7 @@ def result():
     text_total_original_investment = str("{:,.0f}".format(total_original_investment)) + "원"
     text_final_investment = str("{:,.0f}".format(final_investment)) + "원"
 
-    return render_template(r'result.html', total_profit_rate=text_total_profit_rate, total_original_investment= text_total_original_investment, final_investment=text_final_investment)
+    return render_template('result.html', total_profit_rate=text_total_profit_rate, total_original_investment=text_total_original_investment, final_investment=text_final_investment)
 
 if __name__ == '__main__':
     app.run(debug=True)
